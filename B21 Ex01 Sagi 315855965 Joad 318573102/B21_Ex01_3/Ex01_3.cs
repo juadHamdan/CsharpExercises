9bf8c50350e7685544a016cdc12c90ed2c628aff
Sagi Philosof <sagiph@mta.ac.il>
2021-03-28 19:27:45 +0300

    fixed issue - added assembly ref to EX3 from EX2.

--- B21 Ex01 Sagi 315855965 Joad 318573102/B21_Ex01_3/Ex01_3.cs
+++ B21 Ex01 Sagi 315855965 Joad 318573102/B21_Ex01_3/Ex01_3.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Text;
-using static B21_Ex01_2.Ex01_2;
+﻿using static B21_Ex01_2.Ex01_2;
 
 namespace B21_Ex01_3
 {
-     class Ex01_3
+    class Ex01_3
      {
           public static void AdvancedSandClock()
           {
-               B21_Ex01_2.Ex01_2
+               
 
 
           } 
