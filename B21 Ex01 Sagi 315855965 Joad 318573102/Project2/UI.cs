ea403ca5c66e02fac93f800ac525bc027ba40a70
Sagi Philosof <sagiph@mta.ac.il>
2021-04-30 15:52:24 +0300

    Sagi commit

---
+++ B21 Ex01 Sagi 315855965 Joad 318573102/Project2/UI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+    class UI
+    {
+        public void GettingInitValues(int matrixSize)
+        {
+
+        }
+    }
+}
